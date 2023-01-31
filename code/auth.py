9de2eb7205fe08e34ac465caaaf45be780846f7e
Harry Kodden <harry@e-tunity.nl>
2023-01-31 23:51:08 +0100

    updated auth

--- code/auth.py
+++ code/auth.py
@@ -1,19 +1,28 @@
 # auth.py
 
 import os
-from fastapi import status, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import status, HTTPException, Depends, Security
+from fastapi.security import OAuth2PasswordBearer, APIKeyHeader
 
 api_keys = [
     os.environ.get("API_KEY", "secret"),
 ]
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
 
 
-def api_key_auth(api_key: str = Depends(oauth2_scheme)):
-    if api_key not in api_keys:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Forbidden"
-        )
+def api_key_auth(
+    bearer_token: str = Depends(oauth2_scheme),
+    api_key_header: str = Security(api_key_header),
+):
+    if bearer_token in api_keys:
+        return bearer_token
+
+    if api_key_header in api_keys:
+        return api_key_header
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Forbidden"
+    )
